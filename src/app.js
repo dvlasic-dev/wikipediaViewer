897bae7000a29d0c11d8f0f5d05bcdf946acbdf4
Dominik <dvlasic.dev@gmail.com>
2018-06-04 16:24:45 +0200

    changed styles, more code refactor

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
-let btn = document.querySelector('#searchButton');
-let input = document.querySelector('#input');
-let articles = document.querySelector('#articles');
+const input = document.querySelector('#input');
+const articles = document.querySelector('#articles');
 
-function getArticles(inputValue){
-	let req = new XMLHttpRequest();
+function getArticles(inputValue) {
+	const req = new XMLHttpRequest();
 
 	req.open('GET', `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${inputValue}&&origin=*`, true);
 	req.onload = () => {
-		req.status >= 200 && req.status < 400 ? createArticles(JSON.parse(req.response)) : console.error('Request error');
-	}
+		req.status >= 200 && req.status < 400
+			? createArticles(JSON.parse(req.response))
+			: console.error('Request error');
+	};
 	req.send();
 }
 
-function createArticles(data){
-	let headlines = data[1];
-	let descriptions = data[2];
-	let anchors = data[3];
-	let fragment = document.createDocumentFragment();
-	for(let i = 0; data[1].length > i; i++){
-		fragment.append(constructArticle(headlines[i], descriptions[i], anchors[i]));
+function createArticles(data) {
+	const headlines = data[1];
+	const descriptions = data[2];
+	const anchors = data[3];
+	const fragment = document.createDocumentFragment();
+	for (let i = 0; data[1].length > i; i++) {
+		fragment.append(
+			constructArticle(headlines[i], descriptions[i], anchors[i])
+		);
 	}
 	articles.appendChild(fragment);
 }
 
-
 function constructArticle(title, description, link) {
-	let article = document.createElement('article');
-	let heading = document.createElement('h1');
-	let paragraph = document.createElement('p');
-	let url = document.createElement('a');
+	const article = document.createElement('article');
+	const heading = document.createElement('h1');
+	const paragraph = document.createElement('p');
+	const url = document.createElement('a');
 
 	heading.appendChild(document.createTextNode(title));
 	paragraph.appendChild(document.createTextNode(description));
@@ -36,22 +38,39 @@ function constructArticle(title, description, link) {
 	url.target = 'blank';
 	url.text = 'Click here to read more';
 
-	[heading].forEach(arg =>{
+	[heading].forEach(arg => {
 		article.appendChild(arg);
 	});
-	[paragraph].forEach(arg =>{
+	[paragraph].forEach(arg => {
 		article.appendChild(arg);
 	});
-	[url].forEach(arg =>{
+	[url].forEach(arg => {
 		article.append(arg);
 	});
 	article.className = 'article';
 	return article;
 }
 
-document.getElementById('input-form').addEventListener('submit', (e) =>{
-	getArticles(input.value);
-	articles.innerHTML = '';
-	input.innerHTML = '';
+function errorMsg(message) {
+	const div = document.createElement('div');
+	div.appendChild(document.createTextNode(message));
+	div.className = 'error-msg';
+	const container = document.querySelector('.row');
+	const form = document.querySelector('#input-form');
+	container.insertBefore(div, form);
+	setTimeout(() => {
+		document.querySelector('.error-msg').remove();
+	}, 2000);
+}
+
+document.getElementById('input-form').addEventListener('submit', e => {
+	if (input.value === '') {
+		errorMsg('Please fill the input');
+	} else {
+		getArticles(input.value);
+		articles.innerHTML = '';
+		input.value = '';
+	}
+
 	e.preventDefault();
-})
+});
